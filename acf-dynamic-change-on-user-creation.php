d0ccc478c2b0998ec3979d95cef5a5588a160a56
Mike Kipruto <e@kipmyk.co.ke>
2024-06-04 03:22:27 +0300

    Fixed issue #1
    Fixed issue #1

--- acf-dynamic-change-on-user-creation.php
+++ acf-dynamic-change-on-user-creation.php
@@ -2,34 +2,38 @@
 /**
 * Plugin Name: ACF: Dynamic User Role
 * Description: Change default role dynamically on role selection change - ACF role-based conditions on user new / edit form
-* Version: 1.0.7
+* Version: 1.0.8
 * Author: Mike Kipruto
 * Author URI: https://kipmyk.co.ke
 **/
 
-if( ! defined( 'ABSPATH' ) ) exit;
+
+if (!defined('ABSPATH')) {
+    exit; // Exit if accessed directly
+}
 
 class MK_DynamicUserRole
 {
     function __construct() {
         add_action('acf/input/admin_head', array($this, 'acf_dynamic_default_role'));
         add_action('wp_ajax_acf_dynamic_user_role', array($this, 'ajax_acf_dynamic_user_role'));
-      }
+        add_action('wp_ajax_acf_dynamic_default_role', array($this, 'ajax_acf_dynamic_default_role'));
+    }
 
-    function  acf_dynamic_default_role() {
+    function acf_dynamic_default_role() {
         global $pagenow;
-    
-        if ( $pagenow == 'user-new.php' ) {
+
+        if ($pagenow == 'user-new.php') {
             ?>
             <script>
                 jQuery(document).ready(function ($) {
                     $('select#role').on('change', function (e) {
                         $.ajax({
-                            url: '<?php admin_url( 'admin-ajax.php' ); ?>',
+                            url: '<?php echo admin_url('admin-ajax.php'); ?>',
                             type: 'post',
                             data: {
                                 action: 'acf_dynamic_default_role',
-                                security: '<?php wp_create_nonce( 'acf_dynamic_default_role' ) ?>',
+                                security: '<?php echo wp_create_nonce('acf_dynamic_default_role'); ?>',
                                 default_role: e.target.value,
                             },
                             success: function () {
@@ -40,18 +44,18 @@ class MK_DynamicUserRole
                 });
             </script>
             <?php
-        } else if ( $pagenow == 'user-edit.php' ) {
+        } else if ($pagenow == 'user-edit.php') {
             ?>
             <script>
                 jQuery(document).ready(function ($) {
                     $('select#role').on('change', function (e) {
                         $.ajax({
-                            url: '<?php echo admin_url('admin-ajax.php') ?>',
+                            url: '<?php echo admin_url('admin-ajax.php'); ?>',
                             type: 'post',
                             data: {
                                 action: 'acf_dynamic_user_role',
-                                security: '<?php echo wp_create_nonce( 'acf_dynamic_user_role' ) ?>',
-                                user_id: <?php echo sanitize_key($_GET['user_id']) ?>,
+                                security: '<?php echo wp_create_nonce('acf_dynamic_user_role'); ?>',
+                                user_id: <?php echo sanitize_key($_GET['user_id']); ?>,
                                 user_role: e.target.value,
                             },
                             success: function () {
@@ -65,14 +69,33 @@ class MK_DynamicUserRole
         }
     }
 
-    function ajax_acf_dynamic_user_role() {
-        check_ajax_referer( 'acf_dynamic_user_role', 'security' );
-        if ( ( $user_id = sanitize_text_field($_POST['user_id'] )) && ( $user_role = sanitize_text_field($_POST['user_role'] ) )) {
-            $user = new WP_User( $user_id );
-            $user->set_role( $user_role );
+    function ajax_acf_dynamic_default_role() {
+        check_ajax_referer('acf_dynamic_default_role', 'security');
+
+        if (isset($_POST['default_role'])) {
+            $default_role = sanitize_text_field($_POST['default_role']);
+            update_option('default_role', $default_role);
+            wp_send_json_success(__('Default role updated', 'acf'));
+        } else {
+            wp_send_json_error(__('Failed to update default role', 'acf'));
         }
-        wp_redirect( $_SERVER['HTTP_REFERER'] );
+        wp_die();
     }
-   
+
+    function ajax_acf_dynamic_user_role() {
+        check_ajax_referer('acf_dynamic_user_role', 'security');
+
+        if (isset($_POST['user_id']) && isset($_POST['user_role'])) {
+            $user_id = sanitize_text_field($_POST['user_id']);
+            $user_role = sanitize_text_field($_POST['user_role']);
+            $user = new WP_User($user_id);
+            $user->set_role($user_role);
+            wp_send_json_success(__('User role updated', 'acf'));
+        } else {
+            wp_send_json_error(__('Failed to update user role', 'acf'));
+        }
+        wp_die();
+    }
 }
-$mk_DynamicUserRole = new MK_DynamicUserRole(); 
+
+new MK_DynamicUserRole();
